cd98043e79c61c55f08ed611c89a85106fee7e15
Jacqueline Galimany <42303655+jgalsku@users.noreply.github.com>
2023-04-18 02:10:39 -0700

    agrega categoria y vehiculos prueba oracle

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import vehiculo
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,18 @@ def form(request):
 
 def create(request):
     return render(request,'core/create.html')
+
+
+class persona:
+    def __init__(self, nombre, edad):
+        self.nombre=nombre
+        self.edad=edad
+        super().__init__()
+
+
+def home(request):
+    vehiculos = vehiculo.objects.all()
+    datos = {
+        "vehiculos":vehiculos
+    }
+
