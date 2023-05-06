7893152566d21a5818dabbf963570790dab0fbdc
Jacqueline Galimany <42303655+jgalsku@users.noreply.github.com>
2023-05-06 16:07:19 -0700

    creacion tablas Cliente Mascota Producto - sqlite3 db

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+        codigoCliente = models.AutoField(primary_key=True)
+        nombreCliente = models.CharField(max_length=30)
+        telefonoCliente = models.IntegerField()
+        correoCliente = models.CharField(max_length=50)
+        passCliente = models.CharField(max_length=30)
+
+
+        def __str__(self):
+                return self.nombreCliente
+
+class Mascota(models.Model):
+        nombreMascota = models.CharField(max_length=50)
+        nombreCliente = models.ForeignKey(Cliente, on_delete=models.PROTECT)
+
+        def __str__(self):
+                return self.nombreMascota
+
+
+class Producto(models.Model):
+        nombreProducto = models.CharField(max_length=50)
+        precioProducto = models.IntegerField()
+        descripcionProducto = models.TextField()
+        
+        def __str__(self):
+                return self.nombreProducto
+                
