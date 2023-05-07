07cf9055c00eb8b22b6f89ec1582557cba7e9af5
Jacqueline Galimany <42303655+jgalsku@users.noreply.github.com>
2023-05-06 19:39:12 -0700

    realizarregistro agregado

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.urls import URLPattern, path
 from .views import home
 from .views import form
 from .views import create
+from .views import realizarregistro
 
 urlpatterns = [
-    path(' ', home, name="home"),
+    path('', home, name="home"),
     path('home', home, name="home"),
     path('form', form, name="form"),
     path('create', create, name="create"),
+    path('realizarregistro',realizarregistro, name="realizarregistro" )
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Cliente
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -9,3 +11,17 @@ def form(request):
 
 def create(request):
     return render(request,'core/create.html')
+
+def realizarregistro(request):
+    nombre_c = request.POST['nombre']
+    telefono_c = request.POST['telefono']
+    correo_c = request.POST['correo']
+    pass_c = request.POST['password']
+
+    #insert en tabla
+    Cliente.objects.create(nombreCliente = nombre_c,
+                           telefonoCliente = telefono_c,
+                           correoCliente = correo_c,
+                           passCliente = pass_c)
+    messages.success(request, 'Cliente registrado')
+    return redirect('create')
